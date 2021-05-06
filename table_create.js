fe14afa930ef8f9d7174b2b114fb5dc003dc694e
abirhasanmubin <abirhasanmubin@gmail.com>
2021-05-07 01:32:59 +0600

    after parsing and adding data to subsequent row

--- table_create.js
+++ table_create.js
@@ -1,5 +1,8 @@
 let table_config;
 let table_data;
+
+
+// Fetching JSON Data
 fetch('./table_config.json')
     .then(response => {
         response.json().then(data => {
@@ -14,51 +17,108 @@ fetch('./table_data.json')
         });
     });
 
-function createHeader(headerInfo) {
-    let header = document.createElement("h1");
-    let headerText = document.createTextNode(headerInfo);
-    header.appendChild(headerText);
-    return header;
+// Title Creator
+function createTitle(titleInfo) {
+    let title = document.createElement("h1");
+    let titleText = document.createTextNode(titleInfo);
+    title.appendChild(titleText);
+    return title;
 }
 
+// Utility function for parsing Config file 
+// for column header and value_keys of table_data
 function splitConfig(config) {
     let header = [];
-    let value = [];
+    let key = [];
     for (let i = 0; i < config.length; i++) {
         header.push(config[i].header);
-        value.push(config[i].cell);
+        key.push(config[i].cell);
     }
-    return { header, value };
+    return { header, key };
 }
 
-function createColumn(item) {
-    let column = document.createElement("td");
+// Utility function for parsing expected data from table_data
+function parseTableDataColumn(object, key) {
+    // spliting key for ease of search
+    let keys = key.split('.');
+
+    // Safely copying object data
+    let tempObject = { ...object };
+
+
+    for (let i = 0; i < keys.length; i++) {
+        if (tempObject.hasOwnProperty(keys[i])) {
+            if (typeof tempObject[keys[i]] === "object") {
+                tempObject = { ...tempObject[keys[i]] };
+            }
+            else {
+                return tempObject[keys[i]];
+            }
+        }
+        else {
+            return null;
+        }
+    }
+}
+
+// parsing for table row data
+function parseTableDataRow(object, key) {
+    let data = [];
+    for (let i = 0; i < key.length; i++) {
+        data.push(parseTableDataColumn(object, key[i]));
+    }
+    return data;
+}
+
+// Creating Table Column
+function createColumn(item, headerFlag) {
+    let column;
+    if (headerFlag) {
+        column = document.createElement("th");
+    }
+    else {
+        column = document.createElement("td");
+    }
     let columnText = document.createTextNode(item);
     column.appendChild(columnText);
     return column;
 }
 
-function createRow(columnItems) {
+// Creating Table Row
+function createRow(columnItems, headerFlag) {
     let row = document.createElement('tr');
     for (let i = 0; i < columnItems.length; i++) {
-        row.appendChild(createColumn(columnItems[i]));
+        row.appendChild(createColumn(columnItems[i], headerFlag));
     }
     return row;
 }
 
+// Creating Table 
 function createTable(config, data) {
+    //create table and tablebody element
     let table = document.createElement("table");
     let tablebody = document.createElement("tbody");
+
+    // parse config file for creating header
     let results = splitConfig(table_config.columns);
     let columnHeader = results.header;
-    let columnValue = results.value;
-    tablebody.appendChild(createRow(columnHeader));
+    let columnKey = results.key;
+
+    //Add header to the table
+    tablebody.appendChild(createRow(columnHeader, true));
+
+    // parse data for populating table
+    for (let i = 0; i < table_data.length; i++) {
+        let data = parseTableDataRow(table_data[i], columnKey);
+        tablebody.appendChild(createRow(data, false));
+    }
+
     table.appendChild(tablebody);
     return table;
 }
 
 setTimeout(() => {
 
-    document.body.appendChild(createHeader(table_config.title));
+    document.body.appendChild(createTitle(table_config.title));
     document.body.appendChild(createTable(table_config, table_data));
 }, 1000);
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-let obj = {
+let config = {
     "title": "HTML Table",
     "columns": [
         {
@@ -20,6 +20,43 @@ let obj = {
     ]
 }
 
+let data = [
+    {
+        "companyName": "ABC",
+        "contact": {
+            "phone": "0123654",
+            "email": "abc@yopmail.com"
+        },
+        "location": {
+            "country": {
+                "name": "Bangladesh",
+                "code": "BD"
+            },
+            "city": {
+                "name": "Dhaka",
+                "code": "DHK"
+            }
+        }
+    },
+    {
+        "companyName": "DEF",
+        "contact": {
+            "phone": "987123",
+            "email": "def@yopmail.com"
+        },
+        "location": {
+            "country": {
+                "name": "Bangladesh",
+                "code": "BD"
+            },
+            "city": {
+                "name": "Dhaka",
+                "code": "DHK"
+            }
+        }
+    }
+]
+
 function splitConfig(config) {
     let header = [];
     let value = [];
@@ -30,8 +67,31 @@ function splitConfig(config) {
     return { header, value };
 }
 
-let res = splitConfig(obj.columns);
+let res = splitConfig(config.columns);
 
 
 
-console.log(typeof res.header, typeof res.value);
+let header = res.header;
+let value = res.value;
+
+
+function search(obj, val) {
+    let key = val.split(".");
+    let tempObj = { ...obj };
+    for (i = 0; i < key.length; i++) {
+        if (tempObj.hasOwnProperty(key[i])) {
+            if (typeof tempObj[key[i]] != "object") {
+                return tempObj[key[i]];
+            }
+            else {
+                tempObj = { ...tempObj[key[i]] };
+            }
+        }
+        else {
+            return null;
+        }
+    }
+}
+// console.log(value[1]);
+
+console.log(search(data[0], value[0]));
